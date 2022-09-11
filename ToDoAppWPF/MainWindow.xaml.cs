a957fb05d103e643319102e298736283b1b28084
Raevsky Ra <kaneky.ky@outlook.com>
2022-09-11 14:03:45 +0300

    1.0

---
+++ ToDoAppWPF/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToDoAppWPF.Models;
+using ToDoAppWPF.Services;
+
+namespace ToDoAppWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.Json";
+        private BindingList<ToDoModel> _toDoDataList;
+        private FileIOService _fileIOService;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOService = new FileIOService(PATH);
+            try
+            {
+                _toDoDataList = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+            
+            dgTodoList.ItemsSource = _toDoDataList;
+
+            _toDoDataList.ListChanged += _toDoDataList_ListChanged;
+        }
+
+        private void _toDoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged || e.ListChangedType == ListChangedType.ItemAdded)
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                    this.Close();
+                }
+
+                dgTodoList.ItemsSource = _toDoDataList;
+
+                _toDoDataList.ListChanged += _toDoDataList_ListChanged;
+            }
+            
+        }
+    }
+}
---
+++ ToDoAppWPF/Services/FileIOService.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoAppWPF.Models;
+
+namespace ToDoAppWPF.Services
+{
+    internal class FileIOService
+    {
+        private readonly string _filePath;
+        public FileIOService(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public BindingList<ToDoModel> LoadData()
+        {
+            var fileExist = File.Exists(_filePath);
+            if (!fileExist)
+            {
+                File.CreateText(_filePath).Dispose();
+                return new BindingList<ToDoModel>();
+            }
+            using (var reader = File.OpenText(_filePath))
+            {
+                var fileText = reader.ReadToEnd(); 
+                return JsonConvert.DeserializeObject<BindingList<ToDoModel>>(fileText);
+            }
+
+        }
+        public void SaveData(object toDoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(_filePath))
+            {
+                string output = JsonConvert.SerializeObject(toDoDataList);
+                writer.Write(output);
+            }
+        }
+
+    }
+}
